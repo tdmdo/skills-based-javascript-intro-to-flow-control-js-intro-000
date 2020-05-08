14680b4f5eeb54e404c644bbc8f14091eb28a7cb
Daniel Do <daniel.minh.do@gmail.com>
2020-05-08 01:58:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,6 +14,7 @@ function ageChecker(age) {if(age >=13 && age <=19)
 }
 
 function ternaryTeenager(age) {if(age >=13 && age <=19) ? "You are a teenager" : "You are not a teenager"
+}
 
 function switchAge(age) {switch (age)
   {case 13: return "You are a teenager"
